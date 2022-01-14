2025cdb897c52897d27bb9ea0d8fc19c8e509601
Muhamad Hashbi Ashiddiq <muhhashbi@gmail.com>
2022-01-14 14:40:03 +0700

    a: Creating our focus feature

--- App.js
+++ App.js
@@ -1,15 +1,16 @@
 import React, { useState } from "react";
 import { StyleSheet, View } from "react-native";
+import { Focus } from "./src/features/focus/Focus";
 
 export default function App() {
   const [focusSubject, setFocusSubject] = useState(null);
 
-  return <View style={styles.container}>{focusSubject ? <Text>Here is where im going to build a timer</Text> : <Text>Here i want to build an input for a subject</Text>}</View>;
+  return <View style={styles.container}>{focusSubject ? <Text>Here is where im going to build a timer</Text> : <Focus />}</View>;
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 50,
+    backgroundColor: '#252250'
   },
 });
---
+++ src/features/focus/Focus.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+export const Focus = () => {
+  return <View style={styles.container}>
+    <Text>oba</Text>
+  </View>;
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 50,
+  },
+});
